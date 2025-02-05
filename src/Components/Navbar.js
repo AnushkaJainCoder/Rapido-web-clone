8a7edd9dd6bcb31e6b4b60233b6956acb57aa211
anushka.jain <anushka.jain@innovasolutions.com>
2025-02-05 11:53:45 +0530

    "Added Navbar import and component to App.js"

---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+export default function Navbar() {
+    return (
+        <ul>
+            <li>
+                Home
+
+            </li>
+            <li>
+                About Us
+
+            </li>
+            <li>
+                Safety
+
+            </li>
+            <li>
+                Careers
+
+            </li>
+            <li>
+                Blog
+
+            </li>
+            <li>
+                Press
+
+            </li>
+            <li>
+                Contact Us
+            </li>
+        </ul>
+    )
+}
